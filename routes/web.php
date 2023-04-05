9169401564b42282a8df086f4646372b0f196405
eitanmohorade <eitanluc@gmail.com>
2023-04-04 23:24:58 -0300

    repair routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\ProductosController;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\Controller;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +27,52 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+//USUARIOS
+Route::get('/verUsuarios',[UsuarioController::class, 'show'])->name('verUsuarios');
+
+Route::get('agregarUsuario', function () {
+    return view('Eitan.usuarios.agregarUsuario');
+})-> name('agregarUsuario');;
+
+Route::get('editarUsuario', function () {
+    return view('Eitan.usuarios.editarUsuario');
+})->name('editarUsuarios');;
+
+Route::get('editarUsuario/{id}', [UsuarioController::class, 'edit']);
+Route::post('actualizarUsuario/{id}', [UsuarioController::class, 'update']);
+Route::get('eliminarUsuario/{id}', [UsuarioController::class, 'destroy']);
+
+Route::post('/agregarUsuario', [UsuarioController::class, 'store'])-> name('agregarUsuario');
+
+//
+//PRODUCTOS
+Route::get('/verProductos',[ProductosController::class, 'show']);
+
+
+
+Route::post('/agregarProductos', [ProductosController::class, 'store'])->name('agregarProducto');
+
+Route::get('/agregarProductos', function () {
+    return view('Eitan.productos.agregarProductos');
+})->name('agregarProducto');;
+
+Route::get('/agregarProductos',[ProductosController::class, 'showCategorys']);
+//Route::get('/verProductos',[ProductosController::class, 'showCategorys']);
+
+Route::get('editarProducto/{id}', [ProductosController::class, 'edit']);
+Route::post('actualizarProducto/{id}', [ProductosController::class, 'update']);
+Route::get('eliminarProducto/{id}', [ProductosController::class, 'destroy']);
+
+Route::get('editarProducto', function () {
+    return view('Eitan.productos.editarProducto');
+})->name('editarProducto');;
+//
+//CATEGORIAS
+
+Route::get('/agregarVerCategorias',[CategoriaController::class, 'show'])->name('verCategorias');
+
+Route::post('/agregarVerCategorias', [CategoriaController::class, 'store'])->name('agregarCategoria');
+
+Route::get('eliminarCategoria/{id}', [CategoriaController::class, 'destroyC']);
+Route::get('eliminarSubCategoria/{id}', [CategoriaController::class, 'destroySC']);
+Route::get('eliminarSubSubCategoria/{id}', [CategoriaController::class, 'destroySSC']);
