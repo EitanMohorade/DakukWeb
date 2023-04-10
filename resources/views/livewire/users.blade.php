764fa215a57ce303c712de52c5e8316e90096001
Francisco Trujillo <trujilloszitai@gmail.com>
2023-04-10 17:19:15 -0300

    Rendering optimization

--- resources/views/livewire/users.blade.php
+++ resources/views/livewire/users.blade.php
@@ -4,9 +4,9 @@
             <input type="text" placeholder="Buscar..." wire:model="search" class="border-0 rounded-lg w-1/3">
         </div>
     </div>
-    @if (!$users->isEmpty())
     <div class="block w-full overflow-x-auto rounded-t-xl bg-sky-100 shadow-lg">
-            <x-table.table :headers="['ID', 'Nombre', 'Apellido', 'Email', 'Estado']">
+        <x-table.table :headers="['ID', 'Nombre', 'Apellido', 'Email', 'Estado']">
+            @if (!$users->isEmpty())
                 @foreach ($users as $user)
                     <tr class="odd:bg-white even:bg-gray-100" wire:loading.class="opacity-75">
                         <x-table.cell>{{ $user->id }}</x-table.cell>
@@ -38,15 +38,20 @@
                         </x-table.cell>
                     </tr>
                 @endforeach
-            </x-table.table>
+            @else
+                <tr>
+                    <td colspan="6">
+                        <div
+                            class="w-full h-52 bg-white text-center p-4 flex align-center justify-center items-center space-x-3 text-xl text-gray-500">
+                            <x-heroicon-o-information-circle class="w-10 h-10" />
+                            <span>Sin resultados.</span>
+                        </div>
+                    </td>
+                </tr>
+            @endif
+        </x-table.table>
     </div>
-    <div class="p-4 bg-sky-100">
+    <div class="p-4 bg-sky-100 rounded-b-xl">
         {{ $users->links() }}
     </div>
-@else
-    <div class="w-full h-52 bg-white text-center p-4 flex align-center justify-center items-center space-x-3 text-xl text-gray-500 rounded-xl shadow-md">
-        <x-heroicon-o-information-circle class="w-10 h-10 text-gray-500"/>
-        <span>Sin resultados.</span> 
-    </div>
-    @endif
 </div>
